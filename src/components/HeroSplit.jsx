324af9228d502c0c20ade704c0cee9b0c66892a9
jhonSebasdev <jhon.sebasdev@gmail.com>
2026-01-11 19:35:32 -0500

    redeploy to fix vercel

--- src/components/HeroSplit.jsx
+++ src/components/HeroSplit.jsx
@@ -33,7 +33,7 @@ export default function HeroSplit() {
         </div>
       </header>
 
-      {/* ===== DESKTOP SPLIT CON IMÁGENES Y DIAGONAL ===== */}
+      {/* ===== DESKTOP SPLIT CON IMÁGENES Y DIAGONAL   redeploy===== */}
       <div className="hidden md:block absolute inset-0">
         {/* Lado izquierdo - Imagen + overlay morado */}
         <div className="absolute inset-0">

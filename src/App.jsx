c2e3ce79e17a2dbe60d5a78ddaac448d2a45d494
jhonSebasdev <jhon.sebasdev@gmail.com>
2026-01-13 23:21:37 -0500

    Add Google Analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import IntroVideo from "./components/IntroVideo";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Stands from "./sections/Stands";
 import TimelineExperience from "./sections/TimelineExperience";
 import HeroSplit from "./components/HeroSplit";
@@ -8,6 +8,18 @@ import ProjectDetail from "./sections/ProjectDetail";
 import Design from "./sections/Design";
 
 function App() {
+  function Analytics() {
+    const location = useLocation();
+    useEffect(() => {
+      if (window.gtag) {
+        window.gtag("config", "G-XQSBLF6D0", {
+          page_path: location.pathname + location.search,
+          page_title: document.title,
+        });
+      }
+    }, [location.pathname, location.search]);
+    return null;
+  }
   function HomeWithIntro() {
     const [showIntro, setShowIntro] = useState(true);
     return (
@@ -24,13 +36,16 @@ function App() {
   }
 
   return (
-    <Routes>
-      <Route path="/" element={<HomeWithIntro />} />
-      <Route path="/design" element={<Design />} />
-      <Route path="/stands" element={<Stands />} />
-      <Route path="/stands/timeline" element={<TimelineExperience />} />
-      <Route path="/stands/project/:slug" element={<ProjectDetail />} />
-    </Routes>
+    <>
+      <Analytics />
+      <Routes>
+        <Route path="/" element={<HomeWithIntro />} />
+        <Route path="/design" element={<Design />} />
+        <Route path="/stands" element={<Stands />} />
+        <Route path="/stands/timeline" element={<TimelineExperience />} />
+        <Route path="/stands/project/:slug" element={<ProjectDetail />} />
+      </Routes>
+    </>
   );
 }
 
